97f64707c7310a0541ae4752c80e9d61d523732d
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-12-06 03:52:26 +0600

    My Listing (delete) is complete

--- src/components/MyListings.jsx
+++ src/components/MyListings.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
+import { Link } from 'react-router';
 
 const MyListings = () => {
     const [ myDatas, setMyDatas ] = useState([]);
@@ -9,19 +10,34 @@ const MyListings = () => {
         fetch(`http://localhost:3000/listing?email=${user?.email}`)
         .then(res => res.json())
         .then(data => setMyDatas(data))
-    }, [])
+    }, [user?.email])
+    const handleUpdate = (e) => {
+        console.log(e);
+    }
+    const handleDelete = async (id) => {
+        const res = await fetch(`http://localhost:3000/listing/${id}`, {
+            method: "DELETE",
+        })
+        const data = res.json();
+        if(data){
+            const filteredData = myDatas.filter(myData => myData?._id != id)
+            setMyDatas(filteredData);
+        } else {
+            alert("Delete failed" + data.message)
+        }
+    }
     return (
         <div className=' mx-auto '>
-            <div className="overflow-x-auto flex justify-center">
+            <div className="overflow-x-auto flex justify-center text-center">
                 <table className="table">
                     {/* head */}
                     <thead>
                         <tr>
                             <th>Serial No</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>Location & Price</th>
+                            <th>Date</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
 
@@ -34,24 +50,24 @@ const MyListings = () => {
                                         <div className="avatar">
                                             <div className="mask mask-squircle h-12 w-12">
                                                 <img
-                                                    src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                    alt="Avatar Tailwind CSS Component" />
+                                                    src={myData?.image}
+                                                    alt={myData?.name} />
                                             </div>
                                         </div>
                                         <div>
-                                            <div className="font-bold">Hart Hagerty</div>
-                                            <div className="text-sm opacity-50">United States</div>
+                                            <div className="font-bold">{myData?.name}</div>
+                                            <div className="text-sm opacity-50">{myData?.category}</div>
                                         </div>
                                     </div>
                                 </td>
-                                <td>
-                                    Zemlak, Daniel and Leannon
-                                    <br />
-                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                <td className='flex flex-col justify-start'>
+                                    <span>{myData?.location}</span>
+                                    <span className="badge badge-ghost badge-sm">{ myData?.price ? "Price: " : ""}{myData?.price} { myData?.price ? "$" : ""}</span>
                                 </td>
-                                <td>Purple</td>
-                                <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                <td>{myData?.date}</td>
+                                <th className='space-x-2'>
+                                    <button onClick={() => handleUpdate(myData?._id)} className="btn btn-neutral btn-xs">Update</button>
+                                    <button onClick={() => handleDelete(myData?._id)} className="btn btn-warning text-white btn-xs">Delete</button>
                                 </th>
                             </tr>
                         </tbody>)
